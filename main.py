8f59a1f8128a6eee1e88c4dee3bd386595d0493c
paralucks <drakksarge8787452@mail.ru>
2022-12-10 14:47:56 +0300

    Laba1

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+a, b, c = map(float,input().split())
+D=(b**2-4*a*c)
+if D>0:
+    x1=(-b+math.sqrt(D))/2*a
+    x2=(-b-math.sqrt(D))/2*a
+    print('x1 =',x1,'x2 =',x2)
+elif D==0:
+    x1 = (-b + math.sqrt(D)) / 2 * a
+    x2 = x1
+    print('x1 =', x1, 'x2 =', x2)
+else:
+    print('нет корней')
